c729784af0056c930b7d49c36da1523bf1e77149
ernjvr <ernjvr@gmail.com>
2019-01-29 16:55:04 +1300

    refactor: decouple dependency on Android colors & change application color theme

--- app/src/main/java/com/ernjvr/asteroids/data/service/DataService.kt
+++ app/src/main/java/com/ernjvr/asteroids/data/service/DataService.kt
@@ -5,16 +5,17 @@ import com.ernjvr.asteroids.model.Instruction
 object DataService {
 
     val instructions = listOf(
-        Instruction(1, "Asteroids is a game where random asteroids move across the screen."),
-        Instruction(2,"The objective is to navigate your spaceship around the screen and avoid colliding with the moving Asteroids."),
-        Instruction(3,"Press the 'PLAY GAME' button to start the game."),
-        Instruction(4,"When the game starts, asteroids will be moving across the screen."),
-        Instruction(5,"Place your spaceship on the game play area by touching the screen."),
-        Instruction(6,"Move your spaceship around the game play area by touching and dragging the spaceship across the screen."),
-        Instruction(7,"You have 3 lives. When your spaceship collides with an asteroid, you lose a life and the spaceship disappears from the screen."),
-        Instruction(8,"Place your new spaceship on the game play area by touching the screen."),
-        Instruction(9,"When all three lives are lost, the game is over, the Game Over screen will be displayed with the final score and the 'GAME OVER' message."),
-        Instruction(10,"If a high score is achieved, a 'NEW HIGH SCORE!' message will be displayed."),
-        Instruction(11,"Press the 'PLAY GAME' button to start the game to start a new game again.")
+        Instruction("Asteroids is a game where random asteroids move across the screen."),
+        Instruction("The objective is to navigate your spaceship around the screen and avoid colliding with the moving asteroids."),
+        Instruction("One score point is awarded for each successful move."),
+        Instruction("Press the 'PLAY GAME' button to start the game."),
+        Instruction("When the game starts, asteroids will be moving across the screen."),
+        Instruction("Place your spaceship on the game play area by touching the screen."),
+        Instruction("Move your spaceship around the game play area by touching and dragging the spaceship across the screen."),
+        Instruction("You have 3 lives. When your spaceship collides with an asteroid, you lose a life and the spaceship disappears from the screen."),
+        Instruction("Place your new spaceship on the game play area by touching the screen again."),
+        Instruction("When all three lives are lost, the game is over. The final score and the 'GAME OVER' message will display."),
+        Instruction("If a high score is achieved, a 'NEW HIGH SCORE!' message will also be displayed."),
+        Instruction("Press the 'PLAY GAME' button to start another new game.")
     )
 }
--- app/src/main/java/com/ernjvr/asteroids/model/Instruction.kt
+++ app/src/main/java/com/ernjvr/asteroids/model/Instruction.kt
@@ -1,6 +1,6 @@
 package com.ernjvr.asteroids.model
 
-data class Instruction(val number: Int, val description: String) {
+data class Instruction(val description: String) {
 
     override fun toString(): String {
         return description
